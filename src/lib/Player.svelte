9de9ebe843d720e30795eae35e4ff3e42f1b0ada
Lasko Laskov <laskov.lasko@gmail.com>
2023-03-19 14:07:41 +0200

    color changes

--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -5,12 +5,12 @@
     export let score: number;
     export let isPlayerOne: boolean;
 
-    let mainColor = isPlayerOne ? "text-green-500" : "text-red-500";
+    let mainColor = isPlayerOne ? "text-green-700" : "text-red-700";
     let fadeColor = isPlayerOne ? "text-green-300" : "text-red-300";
-    //let mainBGColor = isPlayerOne ? "bg-green-500" : "bg-red-500";
-    let fadeBGColor = isPlayerOne ? "bg-green-50" : "bg-red-50";
+    let mainBGColor = isPlayerOne ? "bg-green-200" : "bg-red-200";
+    let fadeBGColor = isPlayerOne ? "bg-green-100" : "bg-red-100";
     let mainBorderColor = isPlayerOne ? "border-green-500" : "border-red-500";
-    let fadeBorderColor = isPlayerOne ? "border-green-100" : "border-red-100";
+    let fadeBorderColor = isPlayerOne ? "border-green-200" : "border-red-200";
     //base CSS
     const baseCSS = "h-full w-full border-4 rounded-2xl flex justify-center items-center text-3xl"
 
@@ -19,7 +19,7 @@
             ($isPlayerOneTurn && isPlayerOne) ||
             (!$isPlayerOneTurn && !isPlayerOne)
         ) {
-            return `${baseCSS} ${mainBorderColor} ${mainColor} ${fadeBGColor}`;
+            return `${baseCSS} ${mainBorderColor} ${mainColor} ${mainBGColor}`;
         }
         return `${baseCSS} ${fadeBorderColor} ${fadeColor} ${fadeBGColor}`;
     });
